399f55904cc7cc31cd3063d18583ce3382231a6f
brenda-polide <93230723+brenda-polide@users.noreply.github.com>
2021-12-09 23:19:04 -0300

    Add files via upload

---
+++ Desafio tecnologia - Parada 33/lista.py
@@ -0,0 +1,14 @@
+import os
+
+# index     0         1        2        3        4
+lista = ["Lentilha", "Soja Texturalizada", "Ervilha", "Grão de Bico", "Amendoim"]
+
+print(lista)
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+
+os.system("pause")
